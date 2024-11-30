e806466c4fac5eae51e310cdfa6d60b3c7d9e186
KingKrio <jaycool2@student.ubc.ca>
2024-11-30 04:49:47 -0800

    small update

--- mastadon_bot_finder.py
+++ mastadon_bot_finder.py
@@ -4,7 +4,6 @@ from spellchecker import SpellChecker
 from names_dataset import NameDataset
 
 nd = NameDataset()
-checker = SpellChecker(distance=2)
 
 def read_xlsx(path):
     accounts = {}
@@ -31,63 +30,85 @@ def read_xlsx(path):
     return accounts
 
 def find_bots(accounts):
+    criteria=[]
     scrambled_names = []
+
     for user in accounts:
-        if detect_alnum_scrmbl(user["author"]):
-            scrambled_names.append(user["author"])
+        author = (accounts[user])[0]
+        if user not in scrambled_names and is_alnum_scrmbl(author['author'], author['language']):
+            scrambled_names.append(author['author'])
+
+        #for post in accounts[user]:
 
     bots = []
-    return bots
+    return scrambled_names
 
-def detect_alnum_scrmbl(author=""):
-    if author == "": return False
+def is_alnum_scrmbl(author='', lang=''):
+    if lang != 'en': return False
 
+    author = author.replace('-', "_")
+    name_parts = author.split("_") # char_part is short for character parts
+    if len(name_parts) == 0:
+        name_parts = re.findall('[a-zA-Z][^A-Z]*', author)
+    else:
+        for word in name_parts:
+            multi_cased = re.findall('[a-zA-Z][^A-Z]*', word)
+            if len(multi_cased) > 1:
+                i = name_parts.index(word)
+                name_parts = name_parts[:i] + name_parts[i + 1:] + multi_cased
+    name_parts = list(filter(lambda wrd: len(wrd) > 1 or str(wrd).isdigit(), name_parts))
 
-    char_parts = author.split("_") + re.findall('[A-Z][^A-Z]*', author) # char_part is short for character parts
+    print(name_parts)
 
-    # check if there are discernible parts in the author name
-    if len(char_parts) > 1:
-        unknowns = checker.unknown(char_parts)
-        for word in char_parts:
-            size = len(word)
-            is_word_name = [(nd.search(word))[key] for key in ["first_name", "last_name"]].count(None) != 2
-            is_not_valid = not is_word_name and word in unknowns
+    digits = "0123456789"
+    checker = SpellChecker(distance=2)
+    if len(name_parts) > 1: # check if there are discernible parts in the author name
+        unknowns = checker.unknown(name_parts)
+        for word in name_parts:
+            wrd_is_nme = [(nd.search(word))[key] for key in ["first_name", "last_name"]].count(None) != 2
+            if any(char.isdigit() for char in word) and not word.isdigit(): # if we aren't dealing with a num but nums are present
+                word = word.strip(digits)
+                wrd_is_nme = [(nd.search(word))[key] for key in ["first_name", "last_name"]].count(None) != 2
 
-            if not word.isdigit() and any(char.isdigit() for char in word): # if we aren't dealing with a num but nums are present\
-                rm_fst = (word[1:] if word[0].isdigit() else '')
-                rm_lst = (word[:size - 1] if word[size - 1].isdigit() else '')
-                rm_bth = ''.join(sorted(set(rm_fst) & set(rm_lst), key=rm_fst.index))
-
-                if rm_bth != '':
-                    is_word_name = [(nd.search(rm_bth))[key] for key in ["first_name", "last_name"]].count(None) != 2
-                    is_not_valid = not is_word_name and len(checker.unknown([rm_bth])) > 0
-
-                    return is_non_alnum_replacable(rm_bth) if rm_bth.isalnum() else is_not_valid
-
-                if rm_fst == '':
-                    return is_non_alnum_replacable(rm_fst)
-
-                for char in word: # first check if the numbers are only on the end of th
-                    if char.isdigit():
-                        spl_wrd += word.split(char)
-            elif word.isdigit() and len(word) > 1 and len(unknowns) > 1:
+                return (wrd_is_nme or word not in checker.unknown([word])) and not any(char.isdigit() for char in word)
+            elif (word.isdigit() and len(word) > 1 and len(unknowns) > 1) or (not word.isalnum()) or (not wrd_is_nme and word in unknowns):
                 return True
-            elif not word.isalnum():
-                return is_non_alnum_replacable(word)
-            elif is_not_valid:
-                    return True
-            elif not
+    else:
+        word = name_parts[0].strip(digits).lower()
+        if any(char.isdigit() for char in word): return True
 
+        wrd_is_nme = [(nd.search(word))[key] for key in ["first_name", "last_name"]].count(None) != 2
+        if wrd_is_nme or word not in checker.unknown([word]):
+            return False
 
-    return False
+        i = 1
+        while i < len(word):
+            str1, str2 = word[:i], word[i:]
+            unknowns = checker.unknown([str1, str2])
 
-def is_non_alnum_replacable(txt):
-    is_word_name = [(nd.search(txt))[key] for key in ["first_name", "last_name"]].count(None) != 2
-    is_not_valid = not is_word_name and txt in checker.unknown([txt])
+            str1_candidates = [nd.search(str1)[key] for key in ["first_name", "last_name"]]
+            str2_candidates =  [nd.search(str2)[key] for key in ["first_name", "last_name"]]
 
-    if txt.isalnum() and :
+            str1_is_name = str1_candidates.count(None) != 2
+            str2_is_name = str2_candidates.count(None) != 2
+
+            str1_rank = list(filter(None, [list(filter(None, dic['rank'].values())) for dic in str1_candidates if dic is not None and str1_is_name]))
+            str1_rank = max([j for i in str1_rank for j in i]) if len(str1_rank) > 0 else 1001
+
+            str2_rank = list(filter(None, [list(filter(None, dic['rank'].values())) for dic in str2_candidates if dic is not None and str2_is_name]))
+            str2_rank = max([j for i in str2_rank for j in i]) if len(str2_rank) > 0 else 1001
+
+            str1_valid = ((str1_is_name and str1_rank < 1000) or not str1 in unknowns) and len(str1) > 1
+            str2_valid = ((str2_is_name and str2_rank < 1000) or not str2 in unknowns) and len(str2) > 1
+
+            if str1_valid and str2_valid:
+                return False
+            elif str1_valid and not str2_valid:
+                i = 1
+                word = str2
+            i += 1
         return True
-    return True
+    return False
 
 def frequent_hashtags(posts=None):
     if posts is None: return None
@@ -95,10 +116,12 @@ def frequent_hashtags(posts=None):
     return []
 
 def main():
-    path = "//data/defundthepolice-full.xlsx"
+    path = "/home/ybrenin/PycharmProjects/IGS_502_Mastadon_Bot_Finder/data/defundthepolice-full.xlsx"
     account_list = read_xlsx(path)
     test = account_list['Natalia_Army_of_1@mstdn.party']
 
-    print(len(test))
+    #print(test)
+
+    print(find_bots(account_list))
 
 main()
