afe8afead8180f64125012e16908521e33cb15e8
KingKrio <jaycool2@student.ubc.ca>
2024-11-30 22:19:49 -0800

    finished the initial version of the program

--- mastadon_bot_finder.py
+++ mastadon_bot_finder.py
@@ -2,8 +2,11 @@ import re
 import openpyxl
 from spellchecker import SpellChecker
 from names_dataset import NameDataset
+from random_string_detector import RandomStringDetector
 
 nd = NameDataset()
+valid_name = RandomStringDetector(allow_numbers=True)
+checker = SpellChecker(distance=2)
 
 def read_xlsx(path):
     accounts = {}
@@ -28,122 +31,71 @@ def read_xlsx(path):
     return accounts
 
 def find_bots(accounts):
-    scrambled_names, freq_pstr, spams_hstgs = set(), set(), set()
+    scrambled_names, freq_pstr, hstg_spmr = set(), set(), set()
 
     for user in accounts:
         author = (accounts[user])[0]
-
-        if user not in scrambled_names and is_alphanum_scrmbl(author['author'], author['language']):
+        if valid_name(author['author']):
             scrambled_names.add(author['author'])
 
-        if user not in freq_pstr and len(accounts[user]) > 3:
-            dates = {}
+        if spams_hashtags(accounts[user]):
+            hstg_spmr.add(author['author'])
 
-            for post in accounts[user]:
-                date_time = post['date'].split('T')
+        if posts_frequently(accounts[user]):
+           freq_pstr.add(author['author'])
 
-                date = date_time[0]
-                time = (date_time[1]).split(':')[0]
-                if date not in dates:
-                    dates[date] = []
-                (dates[date]).append(time)
+    return {'scrambled names': scrambled_names, 'frequent posters': freq_pstr, 'hashtag spammers': hstg_spmr}
 
-            if posts_frequently(dates):
-                freq_pstr.add(author['author'])
+def posts_frequently(user_posts):
+    dates = {}
 
-    #criteria =
-    bots = scrambled_names.intersection(freq_pstr) #[user for user in accounts if ]
-    return bots
+    for post in user_posts:
+        date_time = post['date'].split('T')
 
-def is_alphanum_scrmbl(author='', lang=''):
-    if lang != 'en': return False
+        date = date_time[0]
+        time = (date_time[1]).split(':')[0]
+        if date not in dates:
+            dates[date] = []
+        (dates[date]).append(time)
 
-    author = author.replace('-', "_")
-    name_parts = author.split("_") # char_part is short for character parts
-    if len(name_parts) == 0:
-        name_parts = re.findall('[a-zA-Z][^A-Z]*', author)
-    else:
-        for word in name_parts:
-            multi_cased = re.findall('[a-zA-Z][^A-Z]*', word)
-            if len(multi_cased) > 1:
-                i = name_parts.index(word)
-                name_parts = name_parts[:i] + name_parts[i + 1:] + multi_cased
-    name_parts = list(filter(lambda wrd: len(wrd) > 1 or str(wrd).isdigit(), name_parts))
-
-    digits = "0123456789"
-    checker = SpellChecker(distance=2)
-    if len(name_parts) > 1: # check if there are discernible parts in the author name
-        unknowns = checker.unknown(name_parts)
-        for word in name_parts:
-            wrd_is_nme = [(nd.search(word))[key] for key in ["first_name", "last_name"]].count(None) != 2
-            if any(char.isdigit() for char in word) and not word.isdigit(): # if we aren't dealing with a num but nums are present
-                word = word.strip(digits)
-                wrd_is_nme = [(nd.search(word))[key] for key in ["first_name", "last_name"]].count(None) != 2
-
-                return (wrd_is_nme or word not in checker.unknown([word])) and not any(char.isdigit() for char in word)
-            elif (word.isdigit() and len(word) > 1 and len(unknowns) > 1) or (not word.isalnum()) or (not wrd_is_nme and word in unknowns):
-                return True
-    elif len(name_parts) == 1:
-        word = name_parts[0].strip(digits).lower()
-        if any(char.isdigit() for char in word): return True
-
-        wrd_is_nme = [(nd.search(word))[key] for key in ["first_name", "last_name"]].count(None) != 2
-        if wrd_is_nme or word not in checker.unknown([word]):
-            return False
-
-        i = 2
-        while i < len(word):
-            str1, str2 = word[:i], word[i:]
-            unknowns = checker.unknown([str1, str2])
-
-            str1_candidates = [nd.search(str1)[key] for key in ["first_name", "last_name"]]
-            str2_candidates = [nd.search(str2)[key] for key in ["first_name", "last_name"]]
-
-            str1_is_name = str1_candidates.count(None) != 2
-            str2_is_name = str2_candidates.count(None) != 2
-
-            str1_rank = list(filter(None, [list(filter(None, dic['rank'].values())) for dic in str1_candidates if dic is not None and str1_is_name]))
-            str1_rank = min([j for i in str1_rank for j in i]) if len(str1_rank) > 0 else 1001
-
-            str2_rank = list(filter(None, [list(filter(None, dic['rank'].values())) for dic in str2_candidates if dic is not None and str2_is_name]))
-            str2_rank = min([j for i in str2_rank for j in i]) if len(str2_rank) > 0 else 1001
-
-            str1_valid = (str1_is_name and str1_rank < 1000 and len(str1) > 2) or (str1 not in unknowns and len(str1) > 1)
-            str2_valid = (str2_is_name and str2_rank < 1000 and len(str2) > 2) or (str2 not in unknowns and len(str2) > 1)
-
-            if str1_valid and str2_valid:
-                return False
-            elif str1_valid and not str2_valid:
-                word = str2
-                i = 2
-            else:
-                i += 1
-        return True
-    return False
-
-def posts_frequently(dates):
     for date in dates:
-        if len(dates[date]) >= 5: # 5 or more posts is frequent
-            return True
-        else: # check if the user has posted frequently
+        if len(dates[date]) >= 4: # 4 or more posts is frequent
             for time in dates[date]:
                 if dates[date].count(time) >= 2:
                     return True
     return False
 
-def frequent_hashtags(posts=None):
-    if posts is None: return None
+def spams_hashtags(user):
+    hashtags = []
 
-    return []
+    for post in user:
+        hashtag_list = list(part[1:] for part in post['text'].split() if part.startswith('#'))
+        [hashtags.append(hashtag) for hashtag in hashtag_list]
+
+    for hashtag in hashtags:#
+        if hashtags.count(hashtag) > 6:
+            return True
+    return False
 
 def main():
     path = "/home/ybrenin/PycharmProjects/IGS_502_Mastadon_Bot_Finder/data/defundthepolice-full.xlsx"
+
     account_list = read_xlsx(path)
-    print(len(account_list))
-    test = account_list['Natalia_Army_of_1@mstdn.party']
+    bots = find_bots(account_list)
 
-    #print(test)
+    print('----Potential bots based on looking for scrambled names: ')
+    for scrambled_name in bots['scrambled names']:
+        print(scrambled_name)
 
-    print(find_bots(account_list))
+    print('\n----Potential bots based on searching for frequent posters: ')
+    for frequent_poster in bots['frequent posters']:
+        print(frequent_poster)
+
+    print('\n----Potential bots based on searching for hashtag spammers: ')
+    for frequent_poster in bots['hashtag spammers']:
+        print(frequent_poster)
+
+    total_bots = len(bots['scrambled names'] | bots['frequent posters'] | bots['hashtag spammers'])
+    print("\n----Total number of potential bots:", total_bots)
 
 main()
